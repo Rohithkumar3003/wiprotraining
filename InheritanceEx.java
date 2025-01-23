6820d5ea262d312bc9a6ce9d057b95a17f8a22b4
Rohithkumar3003 <yellamrajurohith2002@gmail.com>
2025-01-23 15:30:07 +0530

    InheritanceEx.java

--- InheritanceEx.java
+++ InheritanceEx.java
@@ -5,12 +5,22 @@ class Vehicle{
 	}
 }
 class Car extends Vehicle{
-	void {
+	void start(){
+		System.out.println("car is moving");
+	}
 }
-public class InheritanceEx {
+public class InheritanceEx extends Car{
+	
+	void stop() {
+		System.out.println("car stopped");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		InheritanceEx e1 =new InheritanceEx();
+		e1.stop();
+		e1.start();
+		e1.breaks();
 
 	}
 

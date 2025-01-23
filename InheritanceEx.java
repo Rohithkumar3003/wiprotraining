fb8d15df95295d214db1b475c66ca1c7c9aa79fb
Rohithkumar3003 <yellamrajurohith2002@gmail.com>
2025-01-23 15:22:11 +0530

    Add files via upload

---
+++ InheritanceEx.java
@@ -0,0 +1,17 @@
+package com.wiprobasic2;
+class Vehicle{
+	void breaks() {
+		System.out.println("Breaks applied");
+	}
+}
+class Car extends Vehicle{
+	void {
+}
+public class InheritanceEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
